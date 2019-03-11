e46c4079ee71869d6acf6ae3847524c41d97019c
robonetphy <1998patelumang@gmail.com>
2019-03-12 00:24:00 +0530

    Updates

---
+++ precache-manifest.9947a2d1eefb76e2f15b85f21df9f159.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d32e73f90082dc4ed1ad",
+    "url": "/Live-TV/static/css/main.fe06d7a0.chunk.css"
+  },
+  {
+    "revision": "d32e73f90082dc4ed1ad",
+    "url": "/Live-TV/static/js/main.d32e73f9.chunk.js"
+  },
+  {
+    "revision": "928a06a2bb4c37c676ee",
+    "url": "/Live-TV/static/js/runtime~main.928a06a2.js"
+  },
+  {
+    "revision": "3ea618e53c54203acc19",
+    "url": "/Live-TV/static/css/2.475a80b8.chunk.css"
+  },
+  {
+    "revision": "3ea618e53c54203acc19",
+    "url": "/Live-TV/static/js/2.3ea618e5.chunk.js"
+  },
+  {
+    "revision": "9a34e6508646c085377557eaec886656",
+    "url": "/Live-TV/static/media/background.9a34e650.jpg"
+  },
+  {
+    "revision": "77e90fbf5bb7e084380ae1de58d17445",
+    "url": "/Live-TV/index.html"
+  }
+];
